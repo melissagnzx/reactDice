a5021b458d5489722443749de38d7f0fd9b9ddcf
gonzalm7 <gonzalm7@wwu.edu>
2019-07-07 13:40:03 -0700

    finished adding style

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Dice from "./Dice";
+import "./RollDice.css";
 
 class RollDice extends Component {
   static defaultProps = {
@@ -25,9 +26,11 @@ class RollDice extends Component {
 
   render() {
     return (
-      <div>
-        <Dice face={this.state.die1} />
-        <Dice face={this.state.die2} />
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Dice face={this.state.die1} />
+          <Dice face={this.state.die2} />
+        </div>
         <button onClick={this.roll}>Roll Dice</button>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
-
+//Purpose of app is to roll dice by clicking button
 ReactDom.render(<App />, document.getElementById("root"));
