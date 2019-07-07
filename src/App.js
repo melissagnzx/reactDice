73b67a9c9a10d5e64f2f412ad2e104fc53823bf5
gonzalm7 <gonzalm7@wwu.edu>
2019-07-07 13:31:45 -0700

    added props and button to randomize dice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
-import Dice from "./Dice";
+import RollDice from "./RollDice";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dice face="five" />
-        <Dice face="six" />
-        <Dice face="two" />
-        <Dice face="three" />
-        <Dice face="one" />
+        <RollDice />
       </div>
     );
   }
